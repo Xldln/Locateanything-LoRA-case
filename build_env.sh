72893b2ed3f885314db28aac862e04e6d4c9283d
yy <yubinux@yeah.net>
2026-06-08 14:09:46 +0800

    build env bash

---
+++ build_env.sh
@@ -0,0 +1,26 @@
+
+## conda env
+conda create -n lam python=3.10 -y
+conda activate lam
+
+## torch
+pip install torch torchvision --index-url https://download.pytorch.org/whl/cu128
+
+
+## set mirror
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+
+
+## edit deps 
+cd pkg/eaglevl && pip install -e . && cd ../../
+
+
+## other deps
+pip install hf_xet sortedcontainers
+
+
+## download model weights
+
+unset ALL_PROXY all_proxy HTTP_PROXY http_proxy HTTPS_PROXY https_proxy
+
+python download_locate_model.py
---
+++ download_locate_model.py
@@ -0,0 +1,6 @@
+from locateanything_worker import * 
+
+worker = LocateAnythingWorker("nvidia/LocateAnything-3B")
+
+
+print("success download model weights!")
